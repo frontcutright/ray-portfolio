9941f42c99407b178b02b47ebe97f833636cde87
Jelly <raymondcasey@gmail.com>
2023-12-02 04:02:08 +0900

    final

--- components/Header/StartupLogo/Startup.tsx
+++ components/Header/StartupLogo/Startup.tsx
@@ -114,7 +114,7 @@ const Startup = (props) => {
           transition={{ scale: { delay: 1.5, duration: 1.5 } }}
           className="text-AAsecondary font-Text2 text-4xl"
         >
-          A
+          M
         </motion.span>
       </motion.div>
     </motion.div>
